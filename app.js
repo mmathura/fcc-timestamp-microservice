4219f79150fdc37e8e236bc55c5ff04f47eb7382
Glitch (fcc-timestamp-microservic) <none>
2018-02-21 13:33:32 +0000

    👼❤️ Updated with Glitch

--- app.js
+++ app.js
@@ -8,34 +8,34 @@ http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
     
   var s = req.url;
-  console.log("s = " + s); // for debugging
+  // console.log("s = " + s); // for debugging
   
-  var month = ["January", "February", "March", "April", "May", "June", "July", "August", 
-               "September", "October", "November", "December"];
+  var month = ["January", "February", "March", "April", "May", "June", 
+               "July", "August", "September", "October", "November", "December"];
       
   if (s === "/" || s === "/favicon.ico") {
     res.end(null);
   } 
   else {
     var s_url = unescape(s.substr(1)); // remove encoding & trim forward slash  
-    console.log("s_url = " + s_url); // for debugging
+    // console.log("s_url = " + s_url); // for debugging
     var unix_time = Date.parse(s_url); // January 1, 2016
-    console.log("unix_time = " + unix_time); // for debugging
+    // console.log("unix_time = " + unix_time); // for debugging
     if (!Number.isNaN(unix_time)) { // determine if string is not a number
       var d = new Date(Number(unix_time));
-      console.log("d (datestring) = " + d); // for debugging
-      console.log("d.getTime() = " + d.getTime()); // for debugging
+      //console.log("d (datestring) = " + d); // for debugging
+      // console.log("d.getTime() = " + d.getTime()); // for debugging
       if (d.getTime() > 0) {
         // {"unix" : 1451606400, "natural" : "January 1, 2016"}
         var s_date = month[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
-        console.log("s_date = " + s_date); // for debugging
+        // console.log("s_date = " + s_date); // for debugging
         res.end("{\"unix\" : \"" + unix_time + "\", \"normal\" : \"" + s_date + "\"}"); 
       }
     }
     else { // if is a number 
       var d = new Date(Number(s_url)); // 1451606400000
-      console.log("d = " + d); // for debugging
-      if (d > 0) {
+      // console.log("d = " + d); // for debugging
+      if (d > 0) { // create funct to print json and pass Date d 
         res.end("{\"unix\" : \"" + s_url + "\", \"normal\" : \"" + month[d.getMonth()] +  
                 " " + d.getDate() + ", " + d.getFullYear() + "\"}"); 
       }
@@ -44,4 +44,3 @@ http.createServer((req, res) => {
     }
   }
 }).listen(process.env.PORT);
-
